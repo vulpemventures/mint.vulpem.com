daa959d44de0077336ed034a7f143cf9f9b44f7a
tiero <3596602+tiero@users.noreply.github.com>
2021-10-15 11:27:17 +0400

    add modal cofirm

---
+++ src/components/ConfirmModal.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+  import { Status } from '../constants';
+  import SuccessIcon from './icons/SuccessIcon.svelte';
+  import ErrorIcon from './icons/ErrorIcon.svelte';
+
+  export let active: boolean;
+
+  export let txid: string;
+  export let error: string;
+
+  export let status: Status;
+
+  const onCancel = () => {
+    active = false;
+  };
+</script>
+
+<div class="modal {active && 'is-active'}">
+  <div class="modal-background" on:click={onCancel} />
+  <div class="modal-content box has-background-black">
+    <div class="columns">
+      <div class="column is-full has-text-centered mt-6">
+        {#if status === Status.WAITING}
+          <div class="loader-wrapper is-active">
+            <div class="loader is-loading" />
+          </div>
+          <div class="block mt-6 mb-3">
+            <h1 class="title has-text-white">Waiting for Confirmation...</h1>
+            <p class="subtitle is-6 has-text-weight-light has-text-grey">
+              Confirm this transaction in your Marina wallet
+            </p>
+          </div>
+        {:else if status === Status.COMPLETED}
+          <SuccessIcon />
+          <div class="block mt-6 mb-3">
+            <h1 class="title is-3 has-text-white">Issuance Submitted</h1>
+            <a
+              target="_blank"
+              href={'https://blockstream.info/liquid/tx/' + txid}
+            >
+              <p class="subtitle">View on Explorer</p>
+            </a>
+            <button
+              type="button"
+              class="button is-primary is-medium is-rounded mt-6"
+              on:click={onCancel}
+            >
+              Close
+            </button>
+          </div>
+        {:else}
+          <ErrorIcon />
+          <div class="block mt-6 mb-3">
+            <h1 class="title has-text-white">Something went wrong</h1>
+            <p class="subtitle">
+              {error}
+            </p>
+            <button
+              type="button"
+              class="button is-primary is-medium is-rounded mt-6"
+              on:click={onCancel}
+            >
+              Close
+            </button>
+          </div>
+        {/if}
+      </div>
+    </div>
+  </div>
+</div>
--- src/containers/Mint.svelte
+++ src/containers/Mint.svelte
@@ -1,32 +1,48 @@
 <script lang="ts">
+  import ConfirmModal from '../components/ConfirmModal.svelte';
+  import { Status } from '../constants';
 
+  let showModal = false;
+  let modalStatus = Status.WAITING;
+  let error: string;
+  let txid: string;
 
   let name: string;
   let ticker: string;
   let supply: number;
 
   const handleSubmit = () => {
-    console.log(name)
+    console.log(name);
 
     // use name, ticker and assetAmount
     //issueAsset(networks.liquid)(greedyCoinSelector())()
-  }
+  };
 </script>
 
 <form class="box">
- <!--  <h1 class="title">Mint & Sell your NFT</h1> -->
+  <!--  <h1 class="title">Mint & Sell your NFT</h1> -->
   <div class="field">
     <!-- svelte-ignore a11y-label-has-associated-control -->
     <label class="label">Name</label>
     <p class="control">
-      <input class="input" type="text" placeholder="eg. MyToken" bind:value={name} />
+      <input
+        class="input"
+        type="text"
+        placeholder="eg. MyToken"
+        bind:value={name}
+      />
     </p>
   </div>
   <div class="field">
     <!-- svelte-ignore a11y-label-has-associated-control -->
     <label class="label">Ticker</label>
     <p class="control">
-      <input class="input" type="text" placeholder="eg. TKN" bind:value={ticker} />
+      <input
+        class="input"
+        type="text"
+        placeholder="eg. TKN"
+        bind:value={ticker}
+      />
     </p>
   </div>
 
@@ -34,8 +50,14 @@
     <!-- svelte-ignore a11y-label-has-associated-control -->
     <label class="label">Initial Supply</label>
     <p class="control">
-      <input class="input" type="number" placeholder="eg. 100" bind:value={supply} />
+      <input
+        class="input"
+        type="number"
+        placeholder="eg. 100"
+        bind:value={supply}
+      />
     </p>
   </div>
   <button type="button" class="button" on:click={handleSubmit}>Mint</button>
 </form>
+<ConfirmModal bind:active={showModal} status={modalStatus} {txid} {error} />
